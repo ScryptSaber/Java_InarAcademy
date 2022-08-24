4a163355a0a059bee08f818bfc1af373289be4bd
erdem <aerdemkamis@gmail.com>
2022-08-25 01:37:36 +0300

    added new ones

---
+++ src/chapters/chapter03/Exercise03_05.java
@@ -0,0 +1,13 @@
+package chapters.chapter03;
+
+import java.util.Scanner;
+
+public class Exercise03_05 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+	}
+
+}
