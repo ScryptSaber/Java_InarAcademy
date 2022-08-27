1b2d0acba64708d12778c71a1a28543fb9a29c9b
erdem <aerdemkamis@gmail.com>
2022-08-27 14:28:37 +0300

    revising

--- src/chapters/chapter03/Exercise03_34.java
+++ src/chapters/chapter03/Exercise03_34.java
@@ -1,9 +1,32 @@
 package chapters.chapter03;
 
+import java.util.Scanner;
+
 public class Exercise03_34 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter three points for p0, p1, and p2: ");
+		double x0 = input.nextDouble();
+		double y0 = input.nextDouble();
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+
+		double position = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+
+		System.out.print("(" + x2 + ", " + y2 + ") is ");
+
+		if (position == 0) {
+			System.out.print("on the same ");
+
+		} else {
+			System.out.print("not on the same ");
+		}
+
+		System.out.println("line from (" + x0 + ", " + y0 + ") to (" + x1 + ", " + y1 + ")");
 
 	}
 
