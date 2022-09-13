462b7e7cc25803b90dc54d8cf7d89ecba98c615d
erdem <aerdemkamis@gmail.com>
2022-09-13 12:03:09 +0300

    new solutions

---
+++ src/chapters/chapter05/checkpoint05/Triangle.java
@@ -0,0 +1,21 @@
+package chapters.chapter05.checkpoint05;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		int n = 6;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= (n - i) * 2; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = i; k > 0; k--) {
+				System.out.print((k) + " ");
+
+			}
+			System.out.println();
+		}
+	}
+
+}
