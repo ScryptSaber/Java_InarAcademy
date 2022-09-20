d59581f93ca22908b5d9764021f0213b42328c3f
erdem <aerdemkamis@gmail.com>
2022-09-20 22:27:11 +0300

    new solutions

---
+++ src/chapters/chapter06/Exercise06_22.java
@@ -0,0 +1,26 @@
+package chapters.chapter06;
+
+import java.util.Scanner;
+
+public class Exercise06_22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the n: ");
+        long num = input.nextLong();
+        System.out.println("The approximated square root of " + num + " is: " + sqrt(num));
+    }
+
+    public static double sqrt(long n) {
+        long lastGuess = 1;
+        long nextGuess = 2;
+
+        while (nextGuess - lastGuess > 0.0001) {
+            lastGuess = nextGuess;
+            nextGuess = (lastGuess + n / lastGuess) / 2;
+
+        }
+        lastGuess = nextGuess;
+        return nextGuess = (lastGuess + n / lastGuess) / 2;
+    }
+
+}
---
+++ src/chapters/chapter06/Exercise06_23.java
@@ -0,0 +1,28 @@
+package chapters.chapter06;
+
+import java.util.Scanner;
+
+public class Exercise06_23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+
+        System.out.print("Enter char:");
+        String c = input.next();
+
+        System.out.print("Number of occurences " + count(s, c.charAt(0)));
+
+    }
+
+    public static int count(String str, char a) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == a) {
+                count++;
+
+            }
+        }
+        return count;
+    }
+}
