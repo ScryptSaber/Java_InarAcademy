011ecf6827b384e6f84ebb93e0b188e3a38e4cdc
erdem <aerdemkamis@gmail.com>
2022-09-25 17:32:04 +0300

    new solutions

---
+++ src/weeks/week07/Interview_07.java
@@ -0,0 +1,28 @@
+package weeks.week07;
+
+import java.util.Scanner;
+
+public class Interview_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.next();
+
+        if (isUnique(s)) {
+            System.out.println(s + " is unique.");
+        } else {
+            System.out.println(s + " is not unique.");
+        }
+    }
+
+    public static boolean isUnique(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
