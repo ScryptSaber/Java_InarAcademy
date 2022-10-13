5156add2a9fa28eb2fc2983acd9072d7f2fd880a
erdem <aerdemkamis@gmail.com>
2022-10-13 09:41:03 +0300

    new solutions

--- src/chapters/chapter07/Exercise07_33.java
+++ src/chapters/chapter07/Exercise07_33.java
@@ -7,8 +7,8 @@ public class Exercise07_33 {
         Scanner input = new Scanner(System.in);
 
 
-        String[] zodiacs = {"Monkey", "Rooster", "Dog", "pig", "rat", "ox", "tiger",
-                "rabbit", "dragon", "snake", "horse", "sheep"};
+        String[] zodiacs = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger",
+                "Rabbit", "Dragon", "Snake", "Horse", "Sheep"};
 
 
         System.out.print("Enter a year: ");
