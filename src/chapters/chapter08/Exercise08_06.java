f3c50e58fb727e8b4a67753f06fddbad8f10e981
erdem <aerdemkamis@gmail.com>
2022-10-19 09:53:43 +0300

    new solutions added

--- src/chapters/chapter08/Exercise08_06.java
+++ src/chapters/chapter08/Exercise08_06.java
@@ -46,7 +46,7 @@ public class Exercise08_06 {
     }
 
     public static void displayMultiplication(double[][] matrix1, double[][] matrix2, double[][] matrix3) {
-        System.out.println("The multiplication of the matrices is :");
+        System.out.println("The Multiplication of the matrices is :");
         for (int i = 0; i < matrix3.length; i++) {
             for (int j = 0; j < matrix1[i].length; j++) {
                 System.out.print(matrix1[i][j] + " ");
