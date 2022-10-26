08c5d26a0e0125bcf6701aca6e9ef9c257b0cde0
erdem <aerdemkamis@gmail.com>
2022-10-26 12:19:19 +0300

    exercises solved and new solutions added

---
+++ src/chapters/chapter08/Exercise08_37.java
@@ -0,0 +1,19 @@
+package chapters.chapter08;
+
+import java.util.Scanner;
+
+public class Exercise08_37 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number n: ");
+        int n = input.nextInt();
+        char[][] list = new char[n][n];
+
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                System.out.print((char)('A'+j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
