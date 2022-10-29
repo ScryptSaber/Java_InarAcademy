9fdfc9f975b04961ab98878d1a13580de5e24542
erdem <aerdemkamis@gmail.com>
2022-10-29 12:14:49 +0300

    OOP new solutions added

--- src/chapters/chapter09/Exercise02/Exercise09_02.java
+++ src/chapters/chapter09/Exercise02/Exercise09_02.java
@@ -1,4 +1,13 @@
 package chapters.chapter09.Exercise02;
 
 public class Exercise09_02 {
+    public static void main(String[] args) {
+        Stock stock = new Stock("ORCL", "Oracle Corporation");
+        stock.previousClosingPrice = 34.5;
+        stock.currentPrice = 34.35;
+
+        System.out.println("Stock name  : " + stock.name);
+        System.out.println("Stock symbol: " + stock.symbol);
+        System.out.println("Price-change percentage: " + stock.getChangePercent());
+    }
 }
