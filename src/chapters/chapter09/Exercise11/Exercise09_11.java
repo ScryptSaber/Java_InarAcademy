8175fc38cf7d54b448fb27b744244a5dcbabc928
erdem <aerdemkamis@gmail.com>
2022-11-02 10:27:47 +0300

    OOP and Exercises

--- src/chapters/chapter09/Exercise11/Exercise09_11.java
+++ src/chapters/chapter09/Exercise11/Exercise09_11.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercise09_11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a, b, c, d, e, f:  ");
+        System.out.print("Enter a, b, c, d, e, f: ");
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
