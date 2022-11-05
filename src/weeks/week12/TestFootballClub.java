137764bcc3e6f21c3cb32a64d136c8a295b0c50c
erdem <aerdemkamis@gmail.com>
2022-11-05 16:02:22 +0300

    OOP and Exercises

--- src/weeks/week12/TestFootballClub.java
+++ src/weeks/week12/TestFootballClub.java
@@ -2,10 +2,31 @@ package weeks.week12;
 
 public class TestFootballClub {
     public static void main(String[] args) {
-        FootballClub barca = new FootballClub(40_000_000, "Barcelona", 200,
-                "Camp Nou", 150000);
+        Player messi = new Player("Messi", 10_000_000);
+        Player ronaldo = new Player("Ronaldo", 10_000_000);
+        Player maguire = new Player("Maguire", 12_000);
+        Player neymar = new Player("Neymar", 8_000_000);
+        Player mbappe = new Player("Mbappe", 15_000_000);
+        Player haaland = new Player("Haaland", 18_000_000);
+        Player veli = new Player("Veli Kavlak", 100);
+        Player olcay = new Player("Olcay Sahan", 50);
+        Player salah = new Player("Muhammed Salah",5_000_000);
 
-        barca.print();
+        FootballClub dallas_spor = new FootballClub("Dallasspor", 10, 40_000_000l, "Mavericks Stadium", 47856,new Player[5]);
+        dallas_spor.buyPlayer(messi);
+        dallas_spor.buyPlayer(mbappe);
+        dallas_spor.buyPlayer(haaland);
+        dallas_spor.buyPlayer(neymar);
+        dallas_spor.buyPlayer(ronaldo);
+
+        dallas_spor.sellPlayer(messi);
+
+        dallas_spor.buyPlayer(olcay);
+        dallas_spor.buyPlayer(veli);
+        dallas_spor.buyPlayer(maguire);
+        dallas_spor.buyPlayer(neymar);
+
+        System.out.println(dallas_spor.getBalance());
 
     }
 }
