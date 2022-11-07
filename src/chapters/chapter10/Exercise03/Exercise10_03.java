a1e744e660c063ccb9f12663167d1801055f2c1a
erdem <aerdemkamis@gmail.com>
2022-11-08 00:53:10 +0300

    OOP and Exercises

--- src/chapters/chapter10/Exercise03/Exercise10_03.java
+++ src/chapters/chapter10/Exercise03/Exercise10_03.java
@@ -4,8 +4,8 @@ public class Exercise10_03 {
     public static void main(String[] args) {
         MyInteger number = new MyInteger(41614161);
 
-        System.out.println("getValue():  " + number.getValue());
-        System.out.println("isEven():  " + number.isEven());
+        System.out.println("getValue(): " + number.getValue());
+        System.out.println("isEven(): " + number.isEven());
         System.out.println("isOdd(): " + number.isOdd());
         System.out.println("isPrime(): " + number.isPrime());
         System.out.println("equals(10): " + number.equals(1000));
