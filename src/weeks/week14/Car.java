763970c621a90300622b2284feabec93f5a6ab26
erdem <aerdemkamis@gmail.com>
2022-11-13 16:55:18 +0300

    OOP and Exercises

--- src/weeks/week14/Car.java
+++ src/weeks/week14/Car.java
@@ -34,7 +34,7 @@ public class Car extends Vehicle{
     @Override
     public String toString() {
         return super.toString() +
-                "\nPlaka : " + this.plaka +
+                "\nPlaka :  " + this.plaka +
                 "\nMarka : " + this.marka;
     }
 }
