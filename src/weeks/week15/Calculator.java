c9173620820c45b097022d57c5f18fa2d734a347
erdem <aerdemkamis@gmail.com>
2022-11-20 20:14:29 +0300

    OOP Inheritance and Polymorphism is finished

--- src/weeks/week15/Calculator.java
+++ src/weeks/week15/Calculator.java
@@ -1,7 +1,5 @@
 package weeks.week15;
 
-import java.util.ArrayList;
-
 public class Calculator {
     public static Number sum(Number n1, Number n2) {
         return new Number(n1.getValue() + n2.getValue());
