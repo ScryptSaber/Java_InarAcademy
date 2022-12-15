5f4f8fd686cd78fa4ab366b256dd8f0c23dc9721
erdem <aerdemkamis@gmail.com>
2022-12-15 12:45:42 +0300

    List,Set,Map

--- src/chapters/chapter21/map/Exercise21_06.java
+++ src/chapters/chapter21/map/Exercise21_06.java
@@ -35,6 +35,9 @@ public class Exercise21_06 {
             if (entry.getValue() == max) {
                 System.out.print(entry.getKey() + " ");
             }
+
+
+            
         }
     }
 }
